7549bd87bdd0b8f521ac2dae20eb11025569e93c
NicitoNahuel <131872572+NicitoNahuel@users.noreply.github.com>
2023-04-30 16:51:59 -0300

    Update juego del pozo.cpp

--- juego del pozo.cpp
+++ juego del pozo.cpp
@@ -3,8 +3,7 @@
 #include<time.h>
 
 const int n=10;
-int m=10;
-int i, j;
+int i, j, m, vidas;
 int mapa[n][n];
 int posicion[n][n];
 char direccion;
@@ -12,6 +11,7 @@ char pos1 = 'W', pos2 = 'S', pos3='A', pos4='D';
 main()
 {
 	srand(time(NULL));
+//crea el mapa, poniendo 1 en todos los lugares de la matriz
 for (i=0;i<n;i++)
 {
 	for (j=0;j<n;j++)
@@ -19,11 +19,12 @@ for (i=0;i<n;i++)
 	 mapa[i][j]=1;
 	}
 }
-
+//genera 3 numeros aleatorios, estos son los pozos, en los cuales se reeemplaza el 1 por un 0
 for (m=0;m<3;m++)
 	{
 	i= rand() %11;
 	j= rand() %11;
+	//si algun numero esta repetido se vuelve a tirar
 	while (mapa[i][j]==0)
 	{
 	i= rand() %11;
@@ -42,19 +43,39 @@ for (i=0;i<n;i++)
 }
 */
 do{
+printf ("estas en %d %d",i,j);
 printf ("Ingrese la direccion en la que se quiere mover (W) (S) (D) (A)");
 scanf ("%c", &direccion);
+//pos1 W (arriba)
 if (direccion==pos1)
 {j=j+1;}
+//pos2 S (abajo)
 else if(direccion==pos2)
 {j=j-1;}
+//pos3 a (izquierda)
 else if(direccion==pos3)
 {i=i-1;}
+//pos4 d (derecha)
 else if(direccion==pos4)
 {i=i+1;}
-while ("")
+//si caes en un pozo (un 0) te resta 1 vida
+if (posicion[i][j]==0)
+{
+	vidas=vidas-1
+	//si te quedas sin vidas (vidas=0) perdes 
+	if (vidas==0)
+		{printf ("te quedaste sin vidas, el juego termino");}
+	//si todavia tienes vidas empiezas desde 0 0
+	else 
+	{
+		printf ("caiste en un pozo, perdiste una vida, te quedan %d \n empiezas devuelta desde 0 0", vidas);
+		i=0; j=0;
+	}
+}
 
+if (i==(n-1) && j==(n-1))
+{ printf ("felicitaciones, ganaste!!"); }
 
-}while (posicion);
+}while ((i==(n-1) && j==(n-1))|| vidas==0);
 
 }
